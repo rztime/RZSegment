3565ef1baaba4396505dff1e65376ae051319beb
rztime <rex@thecover.co>
2020-04-30 14:02:24 +0800

    Initial Commit

---
+++ RZSegment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RZSegment
+//
+//  Created by ruozui on 2020/4/30.
+//  Copyright © 2020 rztime. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
